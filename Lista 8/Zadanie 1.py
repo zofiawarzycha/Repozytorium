7eb9f08c04abca0228130f5e4869b912277a0f23
Zosia <zofiawarzych01@gmail.com>
2024-12-09 19:26:51 +0100

    Dodanie listy 8 i zadania 1

---
+++ Lista 8/Zadanie 1.py
@@ -0,0 +1,12 @@
+'''
+Napisać program, który dla wprowadzonego przez użytkownika ciągu liczb rzeczywistych wyznacza ich
+średnią arytmetyczną. Wprowadzanie ciągu kończy się poprzez wprowadzenie napisu ’end’. Program
+powinien raportować błąd, jeśli ’end’ jest pierwszą podaną wartością. Przykładowo, dla wejścia:
+1
+-22
+8
+-3.5
+13
+end
+Poprawną odpowiedzią jest -0.7.
+'''
